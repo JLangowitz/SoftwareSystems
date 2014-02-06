2a94602010d35c4873e2a5b16c3eefb7c78162ca
JLangowitz <joshua.langowitz@students.olin.edu>
2014-02-05 20:39:55 -0500

    class work

--- lecture04/aspace.c
+++ lecture04/aspace.c
@@ -9,16 +9,20 @@ License: GNU GPLv3
 #include <stdlib.h>
 
 int global;
+int global2;
 
 int main ()
 {
     int local = 5;
+    int local2 = 1;
     void *p = malloc(128);
 
     printf ("Address of main is %p\n", main);
     printf ("Address of global is %p\n", &global);
+    printf ("Address of global is %p\n", &global2);
     printf ("Address of local is %p\n", &local);
-    printf ("Address of p is %p\n", p);
+    printf ("Address of local2 is %p\n", &local2);
+    printf ("Address of p is %p to %p\n", p, &p[128]);
     
     return 0;
 }
