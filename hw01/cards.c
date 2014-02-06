7f2c36b5d8394dff3aff3401b6816253fbd2b3f3
JLangowitz <joshua.langowitz@students.olin.edu>
2014-02-05 21:40:09 -0500

    refactoring cards.c

--- hw01/cards.c
+++ hw01/cards.c
@@ -7,38 +7,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    char card_name[3];
-    int count = 0;
-    while (card_name[0] != 'X'){
-        puts("Enter the card_name: ");
-        scanf("%2s", card_name);
-        int val = 0;
-        switch(card_name[0]){
-        case 'K':
-        case 'Q':
-        case 'J':
-            val = 10;
+// Globals
+char card_name[3];
+int count = 0;
+
+void get_card_name(){
+    // Prompts for user input of a card name and returns that name
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+}
+
+int get_card_val(char *card_name){
+    // Gets the value of a card name
+    // Returns 10 for a face card, 1 for an ace, 0 for 'X' to end the program.
+    // For other values, returns the atoi of that value.
+    switch(card_name[0]){
+    case 'K':
+    case 'Q':
+    case 'J':
+        return 10;
+    case 'A':
+        return 1;
+    case 'X':
+        return 0;
+    default:
+        return atoi(card_name);
+    }
+}
+
+void update_count(int val){
+    // Takes the inputted card value and updates the card count accordingly
+    // Values from 2-7 increment the count, values of 10 decrement it
+    if ((val > 1) && (val < 8)){
+        count++;
+    } else if (val == 10){
+        count--;
+    }
+    printf("Current count: %i\n", count);
+}
+
+int main(){
+    // Gets card inputs and updates the count until X is entered
+    int val;
+    while(1){
+        get_card_name();
+        val = get_card_val(card_name);
+        if(!val){
             break;
-        case 'A':
-            val = 11;
-            break;
-        case 'X':
+        }
+        if ((val < 1) || (val > 10)){
+            puts("I don't understand that value!");
             continue;
-        default:
-            val = atoi(card_name);
-            if ((val < 1) || (val > 10)){
-                puts("I don't understand that value!");
-                continue;
-            }
         }
-        if ((val > 2) && (val < 7)){
-            count++;
-        } else if (val == 10){
-            count--;
-        }
-        printf("Current count: %i\n", count);
+        update_count(val);
     }
     return 0;
 }
